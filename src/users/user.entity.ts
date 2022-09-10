f4a1a000dcf062f0013305e3ff59d8451bff5d20
RdotSilva <RyandotSilva@gmail.com>
2022-09-10 13:26:06 +0100

    Add custom logging hooks

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,20 @@ export class User {
 
   @Column()
   password: string;
+
+  // Custom hooks that are executed only when creating and using a new Entity instance
+  @AfterInsert()
+  logInsert() {
+    console.log(`Inserted User with ID ${this.id}`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`Updated User with ID ${this.id}`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`Removed User with ID ${this.id}`);
+  }
 }
