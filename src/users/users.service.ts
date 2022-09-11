b982938bbb50885af99a09265ad08eb6b4a721d3
RdotSilva <RyandotSilva@gmail.com>
2022-09-11 19:05:47 +0100

    Add remove method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -31,4 +31,13 @@ export class UsersService {
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
