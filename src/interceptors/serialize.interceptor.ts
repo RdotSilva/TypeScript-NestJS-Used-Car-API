a2ee1e746f474eb28ba2e5ac6d3db9e0857efeff
RdotSilva <RyandotSilva@gmail.com>
2022-09-16 20:41:19 +0100

    Update intercept to convert string to UserDto

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,19 +7,19 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     handler: CallHandler<any>,
   ): Observable<any> {
-    // Run something before the request is handled by request handler
-    console.log('Running before the handler', context);
-
     return handler.handle().pipe(
       map((data: any) => {
-        // Run something before the response is sent out
-        console.log('Running before the response is sent out.', data);
+        // Convert the incoming data to a user data object before the response is sent out
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true, // Only share properties that are marked with the @Expose annotation
+        });
       }),
     );
   }
