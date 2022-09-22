18df359b15a4b6fce4bb23dbac8a25e77680e12f
RdotSilva <RyandotSilva@gmail.com>
2022-09-22 18:52:44 -0400

    Remove example for colors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,11 +26,6 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
-  @Get('/colors/:color')
-  setColor(@Param('color') color: string, @Session() session: any) {
-    session.color = color;
-  }
-
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.authService.signup(body.email, body.password);
