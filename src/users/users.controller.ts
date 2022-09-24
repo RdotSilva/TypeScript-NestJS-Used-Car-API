94398321faa701a7b4779c556a1363235a5784a2
RdotSilva <RyandotSilva@gmail.com>
2022-09-23 20:50:16 -0400

    Add signOut post method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -30,7 +30,12 @@ export class UsersController {
   whoAmI(@Session() session: any) {
     return this.usersService.findOne(session.userId);
   }
-  
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signup(body.email, body.password);
