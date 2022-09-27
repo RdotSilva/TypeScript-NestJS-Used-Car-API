77f0a29d1814ff5c4f8cceb911f7b33db342df8e
RdotSilva <RyandotSilva@gmail.com>
2022-09-27 15:51:06 -0400

    Refactor test to use fakeUsersService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,8 +3,21 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
 it('can create an instance of auth service', async () => {
+  // Create fake copy of UsersService
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+  };
+
   const module = await Test.createTestingModule({
-    providers: [AuthService],
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
   }).compile();
 
   // Create new instance of AuthService with all dependencies initialized
