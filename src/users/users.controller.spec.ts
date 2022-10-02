42799a72865256720de51cd1c4ad8e6a693a137a
RdotSilva <RyandotSilva@gmail.com>
2022-10-02 20:06:19 +0100

    Add Partial mock services

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,45 @@
+import { User } from './user.entity';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    // Mocked fake services to use within the tests
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id: 9999,
+          email: 'test@test.com',
+          password: 'test123!',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: 'test123' } as User]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      // Pass in the mock services we created above
+      providers: [
+        { provide: UsersService, useValue: fakeUsersService },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
