d67c96dd5d9fc7f014e80c90d65604a1f75fa9c6
RdotSilva <RyandotSilva@gmail.com>
2022-10-12 17:49:44 +0100

    Update create to add association of user to report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,13 +3,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportDto: CreateReportDto) {
+  create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
+
+    // Add association of user to the report
+    report.user = user;
 
     return this.repo.save(report);
   }
